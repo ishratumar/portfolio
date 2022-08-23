8b1f8bd598e171bce1508f76f70b8d9ea38927fa
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-08-23 19:06:35 +0500

    added some minor changes

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Content Creator", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Content Creator", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -71,3 +71,5 @@ $(document).ready(function(){
         }
     });
 });
+const year = document.querySelector('.year');
+year.innerHTML = new Date().getFullYear();
